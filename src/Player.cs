ab28b12f9484e430e8f0ac4a10e48119b474c873
Наумов Юрий <ynaumov@mail.ru>
2022-10-18 18:08:18 +0300

    Создали основного игрока

--- src/Player.cs
+++ src/Player.cs
@@ -1,30 +1,31 @@
-public class Player
+namespace RPGClasses
 {
-    private string _name;
-    public string Name
+    public class Player
     {
-        get { return _name; }
-        set { _name = value; }
-    }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
 
-    private int _health;
-    public int Health
-    {
-        get { return _health; }
-        set { _health = value; }
-    }
-    
+        private int _health;
+        public int Health
+        {
+            get { return _health; }
+            set { _health = value; }
+        }        
 
-    public Player(string name, int health)
-    {
-        Name = name;
-        Health = health;
+        public Player(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
 
+        public void KnockOut()
+        {
+            if (Health == 0) Console.WriteLine("Персонаж оглушен");
+        }
+        
     }
-
-    public void KnockOut()
-    {
-        If (Health == 0) Console.WriteLine("Персонаж оглушен");
-    }
-    
 }
