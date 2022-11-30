ce8fa1e28bab95c08bff89ca86703239f380228a
Наумов Юрий <ynaumov@mail.ru>
2022-11-30 10:05:53 +0300

    Weapon: начальные данные

---
+++ src/Weapon.cs
@@ -0,0 +1,13 @@
+namespace RPGClasses
+{
+    public class Weapon
+    {
+        public int Damage { get; set; }
+        public string Name { get; set; }    
+        public Weapon()
+        {
+            Damage =1;
+            Name = "Кинжал";
+        }
+    }
+}
